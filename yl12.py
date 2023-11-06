c059ca436125be86f4de41f58f4d6bae8fef31f8
silverreinart1 <145542354+silverreinart1@users.noreply.github.com>
2023-11-06 13:13:01 +0200

    Update yl12.py

--- yl12.py
+++ yl12.py
@@ -1,17 +1,3 @@
-thislist = ('õun', 'banaan', 'apelsin')
-print(thislist[0])
-
-thislist2 = ('õun', 'banaan', 'apelsin', 'pirn')
-print(thislist2[3])
-
-thislist3 = ('õun, mango, apelsin, pirn')
-print(thislist3)
-
-thislist4 = ('õun', 'mango', 'apelsin', 'pirn')
-print(len(thislist4))
-
-thislist5 = ('õun, mango, apelsin')
-print(thislist5)
-
-thislist6 = ('apelsin, mango, õun')
-print(thislist6)
+thislist = ["õun", "banaan", "apelsin"]
+thislist.append("aprikoos")
+print(thislist)
