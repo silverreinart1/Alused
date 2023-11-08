7e8e304346059be01ff8a0c29ae802e23a5c316d
silverreinart1 <145542354+silverreinart1@users.noreply.github.com>
2023-11-08 14:50:14 +0200

    Update yl17.py

--- yl17.py
+++ yl17.py
@@ -2,7 +2,7 @@ string = input('Sisesta tekst:')
 vowels = 0
 
 for i in string:
-      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='õ' or i=='ä' or i=='ö' or i=='ü' ):
             vowels=vowels+1
 
 print(vowels)
