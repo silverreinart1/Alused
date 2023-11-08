9d81c57c4b88eba2d5b423b075a249ca5609e7bd
silverreinart1 <145542354+silverreinart1@users.noreply.github.com>
2023-11-08 15:31:37 +0200

    Update yl20.py

--- yl20.py
+++ yl20.py
@@ -1,13 +1,29 @@
 import random
-num = random.randint(1, 100)
-quess = None
 
-while quess != num:
-    quess = input("Arva number 1-100: ")
-    quess = int(quess)
+while True:
+    user_action = input("Kas (kivi, paber, käärid)?: ")
+    possible_actions = ["kivi", "paber", "käärid"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
-    if quess == num:
-        print("Õige JAAAAA")
+    if user_action == computer_action:
+        print(f"vali {user_action}. See oli viik! ")
+    elif user_action == "kivi":
+        if computer_action == "käärid":
+            print("Sa oled võitja! WWW")
+        else:
+            print("Anlaki! LLL")
+    elif user_action == "paber":
+        if computer_action == "kivi":
+            print("Sa oled võitja! WWW")
+        else:
+            print("Anlaki! LLL")
+    elif user_action == "käärid":
+        if computer_action == "paber":
+            print("Sa oled võitja! WWW")
+        else:
+            print("Anlaki! LLL")
+
+    play_again = input("Uuesti? (Jah/Ei): ")
+    if play_again.lower() != "Jah":
         break
-    else:
-        print("VALE!!!!!! :(")
