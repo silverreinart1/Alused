30ee935ac318aa468b7aac17a72c9e57c0a56a30
silverreinart1 <145542354+silverreinart1@users.noreply.github.com>
2023-11-20 11:35:04 +0200

    Update yl25.py

--- yl25.py
+++ yl25.py
@@ -6,5 +6,24 @@ mina = {
   "Lemmik magustoit": "Kook",
 }
 
-x = mina.get("Elukoht")
-print(x)
+print(mina.get('Elukoht'))
+
+mina['Lemmik magustoit'] = 'Kook'
+
+mina['height'] = '1.80'
+
+del mina['Sünniaasta']
+
+mina.clear()
+
+for s, r in mina.items():
+    print(s, r)
+
+#me['personal_code'] = '12345678910'
+
+if 'personal_code' in mina:
+    print(mina['Isikukood on dictionaris '])
+else:
+    print('Isikukood ei ole dictionaris')
+
+print(len(mina))
